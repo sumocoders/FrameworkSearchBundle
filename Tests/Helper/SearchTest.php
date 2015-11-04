a8dd8233764309efdd52a95da99efb173708d72e
woutersioen <wouter.sioen@gmail.com>
2015-11-04 16:59:46 +0100

    Add extra tests for sorting and no results.

--- Tests/Helper/SearchTest.php
+++ Tests/Helper/SearchTest.php
@@ -12,13 +12,9 @@ class SearchTest extends \PHPUnit_Framework_TestCase
         $sqlResult = array(
             '\Some\Class' => array(12 => 2),
         );
-        $eventResult = array(new SearchResult(
-            '\Some\Class',
-            12,
-            'TestBundle',
-            'Test',
-            'test'
-        ));
+        $eventResult = array(
+            new SearchResult('\Some\Class', 12, 'TestBundle', 'Test', 'test')
+        );
         $repository = $this->getRepositoryMock($sqlResult);
         $eventDispatcher = $this->getEventDispatcherMock($eventResult);
 
@@ -36,6 +32,74 @@ class SearchTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($eventResult[0]->getId(), $results[0]->getId());
         $this->assertEquals($eventResult[0]->getClass(), $results[0]->getClass());
         $this->assertEquals($eventResult[0]->getTitle(), $results[0]->getTitle());
+    }
+
+    public function testSearchNoEventResults()
+    {
+        $sqlResult = array(
+            '\Some\Class' => array(12 => 2),
+        );
+        $eventResult = array();
+        $repository = $this->getRepositoryMock($sqlResult);
+        $eventDispatcher = $this->getEventDispatcherMock($eventResult);
+
+        $search = new Search($repository, $eventDispatcher, 'test');
+        $search->search();
+        $results = $search->getResults();
+
+        // our item does not exist anymore
+        $this->assertCount(0, $results);
+    }
+
+    public function testSearchWrongSorting()
+    {
+        $sqlResult = array(
+            '\Some\Class' => array(3 => 1, 12 => 2),
+        );
+        $eventResult = array(
+            new SearchResult('\Some\Class', 3, 'TestBundle', 'Test', 'test'),
+            new SearchResult('\Some\Class', 12, 'TestBundle', 'Test', 'test')
+        );
+        $repository = $this->getRepositoryMock($sqlResult);
+        $eventDispatcher = $this->getEventDispatcherMock($eventResult);
+
+        $search = new Search($repository, $eventDispatcher, 'test');
+        $search->search();
+        $results = $search->getResults();
+
+        // our items should still be here, because they are found in both sql and event
+        $this->assertCount(2, $results);
+
+        // the order should be from high weight to low weight
+        $this->assertEquals(2, $results[0]->getWeight());
+        $this->assertEquals(1, $results[1]->getWeight());
+    }
+
+    public function testSearchCorrectSorting()
+    {
+        $sqlResult = array(
+            '\Some\Class' => array(3 => 1, 4 => 1),
+            '\Other\Class' => array(12 => 2),
+        );
+        $eventResult = array(
+            new SearchResult('\Some\Class', 3, 'TestBundle', 'Test', 'test'),
+            new SearchResult('\Some\Class', 4, 'TestBundle', 'Test', 'test'),
+            new SearchResult('\Other\Class', 12, 'TestBundle', 'Test', 'test')
+        );
+        $repository = $this->getRepositoryMock($sqlResult);
+        $eventDispatcher = $this->getEventDispatcherMock($eventResult);
+
+        $search = new Search($repository, $eventDispatcher, 'test');
+        $search->search();
+        $results = $search->getResults();
+
+        // our items should still be here, because they are found in both sql and event
+        $this->assertCount(3, $results);
+
+        // the order should be from high weight to low weight
+        $this->assertEquals(2, $results[0]->getWeight());
+        $this->assertEquals(1, $results[1]->getWeight());
+        $this->assertEquals(1, $results[2]->getWeight());
     }
 
     private function getRepositoryMock($sqlResult)
