da35457a9f62dc92adc46cda4f174dffb0fa26f6
Tijs Verkoyen <tijs@verkoyen.eu>
2016-03-02 16:54:46 +0100

    Stop using deprecated stuff

--- Form/Type/SearchType.php
+++ Form/Type/SearchType.php
@@ -5,6 +5,7 @@ namespace SumoCoders\FrameworkSearchBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -34,20 +35,15 @@ class SearchType extends AbstractType
             );
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(
             array(
                 'horizontal' => false,
             )
         );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getName()
-    {
-        return '';
     }
 }
